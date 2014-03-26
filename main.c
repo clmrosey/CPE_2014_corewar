a05f4c6bbbad7fb7478330f1fdda3b4ea2db71c7
rosey_c <rosey_c@epitech.eu>
2014-03-26 16:27:15 +0100

    1er

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for main in /home/rosey/rendu/CPE_2014_corewar
+** 
+** Made by rosey
+** Login   <rosey@epitech.net>
+** 
+** Started on  Wed Mar 26 16:25:16 2014 rosey
+** Last update Wed Mar 26 16:25:32 2014 rosey
+*/
+
+int	main()
+{
+  return (o);
+}
