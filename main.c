df9eacb8d7e23411423f6c40b0c6c0a37f522f12
rosey_c <rosey_c@epitech.eu>
2014-03-26 16:41:56 +0100

    modif

--- main.c
+++ main.c
@@ -5,12 +5,10 @@
 ** Login   <rosey@epitech.net>
 ** 
 ** Started on  Wed Mar 26 16:25:16 2014 rosey
-** Last update Wed Mar 26 16:37:08 2014 rosey
+** Last update Wed Mar 26 16:40:55 2014 rosey
 */
 
 int	main()
 {
   return (o);
 }
-
-coucou
