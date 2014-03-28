8240195f1714755055077d0a48d619377faa0319
rosey_c <rosey_c@epitech.eu>
2014-03-28 09:11:00 +0100

    modif

--- asm/my.h
+++ asm/my.h
@@ -5,6 +5,11 @@
 ** Login   <rosey@epitech.net>
 ** 
 ** Started on  Fri Mar 28 09:06:18 2014 rosey
-** Last update Fri Mar 28 09:06:38 2014 rosey
+** Last update Fri Mar 28 09:10:21 2014 rosey
 */
 
+#ifndef MY_H
+# define MY_H
+
+
+#endif /* !MY_H_*/
