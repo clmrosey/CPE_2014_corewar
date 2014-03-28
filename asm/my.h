257b76b1b0874e5b158895c0d8941cdf273af27f
rosey_c <rosey_c@epitech.eu>
2014-03-28 09:07:16 +0100

    modif

---
+++ asm/my.h
@@ -0,0 +1,10 @@
+/*
+** my.h for my in /home/rosey/rendu/CPE_2014_corewar/asm
+** 
+** Made by rosey
+** Login   <rosey@epitech.net>
+** 
+** Started on  Fri Mar 28 09:06:18 2014 rosey
+** Last update Fri Mar 28 09:06:38 2014 rosey
+*/
+
